50b936461b255a180ef7768ab26f0f3f645ddbd1
Tyler Hunter <tyler.a.hunter@gmail.com>
2018-04-24 20:30:28 -0400

    index

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var myText = 'I am a string';
+var newString = myText.replace('string', 'strong');
+console.log(newString);
+// the replace() string function takes a string,
+// replaces one substring with another, and returns
+// a new string with the replacement made
